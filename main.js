1b1283a2f7a0ba712883d45072b8f17f150edbe1
oltishabani <oltishabani23@gmail.com>
2023-10-30 10:05:20 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,18 @@
+const startBtn = document.querySelectort('.start-btn');
+const popupInfo = document.querySelectort('.popup-info');
+const exitBtn = document.querySelector('.exit-btn');
+const main = document.querySelector('.main');
+const continueBtn = document.querySelector('.continue-btn');
+const quizSection = document.querySelector('.quiz-section');
+
+
+startBtn.onclick = () => {
+    popupInfo.classList.add('active');
+    main.classList.add('active');
+}
+
+exitBtn.onclick = () => {
+    popupInfo.classList.remove('active');
+    main.classList.remove('active');
+}
+
